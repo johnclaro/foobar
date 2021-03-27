6537dc6db8a58cddf664d3d4848c307e4e42d1ce
John Claro <jkrclaro@gmail.com>
2021-03-27 20:36:08 +0000

    Nodes and edges

--- c2.py
+++ c2.py
@@ -1,43 +1,45 @@
-from termcolor import colored
+from collections import namedtuple
 
-
-moves = (
-        (-1, 2), (1, 2),
-    (-2, 1),            (2, 1),
-    (-2, -1),            (2, -1),
-        (-2, -2), (1, -2),
-)
+Node = namedtuple('Node', ['x', 'y'])
 
 
 def solution(src, dest):
     rows, cols = (8,8)
     board = init_board(rows, cols)
-    output = 0
-    for move in moves:
-        move_x, move_y = move
-        src_x, src_y = board[src]
-        dest_x, dest_y = board[dest]
-        diff_x = move_x + src_x
-        diff_y = move_y + src_y
-        if (0 < diff_x < 8) and (0 > diff_y > -8):
-            diff = (diff_x, diff_y)
-            for cell, coordinate in board.items():
-                if coordinate == diff and coordinate == board[dest]:
-                    output += 1
-    return output
+    edges = generate_edges(board)
+    return 0
 
 
 def init_board(rows, cols):
     board = {}
-    cell = 0
+    square = 0
     for col in range(cols):
         for row in range(rows):
-            coordinate = (row, -col)
-            board[cell] = coordinate
-            cell += 1
+            node = Node(row, -col)
+            board[node] = square
+            square += 1
     return board
 
 
+def generate_edges(board):
+    edges = {}
+    moves = (
+            Node(-1, 2), Node(1, 2),
+        Node(-2, 1),            Node(2, 1),
+        Node(-2, -1),            Node(2, -1),
+            Node(-2, -2), Node(1, -2),
+    )
+    for node in board.keys():
+        for move in moves:
+            x, y = move.x + node.x, move.y + node.y
+            if (0 < x < 8) and (0 > y > -8):
+                edge = (x, y)
+                try:
+                    edges[node].append(edge)
+                except KeyError:
+                    edges[node] = [edge]
+    return edges
+
 if __name__ == '__main__':
-    output = solution(0, 1)
+    output = solution(19, 36)
     print output
