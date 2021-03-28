49c3273a6bfdb3e84c1b82df8f2e83c5261d7fd9
John Claro <jkrclaro@gmail.com>
2021-03-28 22:40:33 +0100

    Update variables

--- c2.py
+++ c2.py
@@ -2,29 +2,19 @@ import time
 from collections import deque
 
 
-def solution(src, dest):
-    rows, cols = (8, 8)
-    board = init_board(rows, cols)
-    graph = init_graph(board)
-    path = find_shortest_path(graph, src, dest)
-    output = len(path) - 1
-    return output
-
-
 def init_board(rows, cols):
     board = {}
     node = 0
-    for col in range(cols):
-        for row in range(rows):
-            point = (row, -col)
-            board[point] = node
+    for y in range(cols):
+        for x in range(rows):
+            board[x, -y] = node
             node += 1
     return board
 
 
 def init_graph(board):
     graph = {}
-    moves = (
+    lshapes = (
         (-1, 2),
         (-2, 1),
         (1, 2),
@@ -35,24 +25,25 @@ def init_graph(board):
         (1, -2),
     )
     for point, node in board.items():
-        for move in moves:
-            x, y = move[0] + point[0], move[1] + point[1]
-            if (0 <= x < 8) and (0 >= y > -8):
-                edge = board[(x, y)]
+        for lshape in lshapes:
+            x = lshape[0] + point[0]
+            y = lshape[1] + point[1]
+            if 0 <= x < 8 and 0 >= y > -8:
+                adjacent = board[x, y]
                 try:
-                    graph[node].append(edge)
+                    graph[node].append(adjacent)
                 except KeyError:
-                    graph[node] = [edge]
+                    graph[node] = [adjacent]
     return graph
 
 
 def find_shortest_path(graph, src, dest):
     root = [[src]]
-    nodes = deque(root)
+    queue = deque(root)
     visited = set()
-    while nodes:
-        # Get the first path in the nodes
-        path = nodes.popleft()
+    while queue:
+        # Get the first path in the queue
+        path = queue.popleft()
         # Get the last node in the path
         node = path[-1]
         # Stop if destination is reached
@@ -60,14 +51,24 @@ def find_shortest_path(graph, src, dest):
             return path
         # Check current node if visited in order not to recheck it
         elif node not in visited:
-            # Loop adjacent nodes then construct new path and add to nodes
-            for adj_node in graph[node]:
+            # Loop adjacent node then construct new path and add to queue
+            for adjacent_node in graph[node]:
+                # TODO: Why is this list(path) and not [path]?
                 paths = list(path)
-                paths.append(adj_node)
-                nodes.append(paths)
+                paths.append(adjacent_node)
+                queue.append(paths)
             # Mark the node as visited
             visited.add(node)
-    return nodes
+    return queue
+
+
+def solution(src, dest):
+    rows, cols = (8, 8)
+    board = init_board(rows, cols)
+    graph = init_graph(board)
+    path = find_shortest_path(graph, src, dest)
+    output = len(path) - 1
+    return output
 
 
 if __name__ == '__main__':
